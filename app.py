2677989734c4496630f68f8d02e31a68a35a60c3
Gabriela Mendes <gabi06marquesmd@gmail.com>
2024-05-30 18:54:57 -0300

    Improved code translation to English

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = ''
+API_KEY = 'test_66e02a7647ab45ca54f819fab08840'
 BASE_URL = 'https://api.api-futebol.com.br/v1/'
 
-def get_all_times_data():
-    all_times_data = []
+def get_all_teams_data():
+    all_teams_data = []
 
     for time_id in range(10):
         url = BASE_URL + f'times/{time_id}'
@@ -16,11 +16,11 @@ def get_all_times_data():
         response = requests.get(url, headers=headers)
         data = response.json()
 
-        all_times_data.append(data)
+        all_teams_data.append(data)
 
-    return all_times_data
+    return all_teams_data
 
-def get_time_data_by_id(time_id):
+def get_team_data_by_id(time_id):
     url = BASE_URL + f'times/{time_id}'
     headers = {'Authorization': f'Bearer {API_KEY}'}
     response = requests.get(url, headers=headers)
@@ -31,38 +31,38 @@ def index():
     return render_template('index.html')
 
 @app.route('/times/all')
-def get_all_times():
-    all_times_data = get_all_times_data()
-    return render_template('times.html', times=all_times_data)
+def get_all_teams():
+    all_teams_data = get_all_teams_data()
+    return render_template('times.html', times=all_teams_data)
 
 @app.route('/times/<int:id_time>', methods=['GET'])
 def get_time_by_id(id_time):
-    time_data = get_time_data_by_id(id_time)
-    if 'erro' in time_data:
-        return render_template('time_especifico.html', error='Time não encontrado')
-    return render_template('time_especifico.html', time=time_data)
+    team_data = get_team_data_by_id(id_time)
+    if 'erro' in team_data:
+        return render_template('time_especifico.html', error='Team not found')
+    return render_template('time_especifico.html', time=team_data)
 
 @app.route('/times/escudo/<int:id_time>', methods=['GET'])
 def get_escudo(id_time):
-    all_times_data = get_all_times_data()
+    all_teams_data = get_all_teams_data()
 
-    for time_data in all_times_data:
-        if time_data['time_id'] == id_time:
-            escudo_url = time_data['escudo']
+    for team_data in all_teams_data:
+        if team_data['time_id'] == id_time:
+            escudo_url = team_data['escudo']
             return jsonify({'escudo_url': escudo_url})
 
-    return jsonify({'error': 'Time não encontrado'}), 404
+    return jsonify({'error': 'Team not found'}), 404
 
 @app.route('/times/escudo/imagem/<int:id_time>', methods=['GET'])
 def redirect_to_escudo(id_time):
-    all_times_data = get_all_times_data()
+    all_teams_data = get_all_teams_data()
 
-    for time_data in all_times_data:
-        if time_data['time_id'] == id_time:
-            escudo_url = time_data['escudo']
+    for team_data in all_teams_data:
+        if team_data['time_id'] == id_time:
+            escudo_url = team_data['escudo']
             return redirect(escudo_url)
 
-    return jsonify({'error': 'Time não encontrado'}), 404
+    return jsonify({'error': 'Team not found'}), 404
 
 @app.route('/times/time', methods=['GET'])
 def search_time_by_id():
